0a23195cddce1619c9f9866f613ea01665b2f25e
undergroundenemy616 <undergroundenemy616@yandex.ru>
2024-06-25 09:20:27 +0300

    fixes

--- delivery/core/domain/model/shared_kernel/location.py
+++ delivery/core/domain/model/shared_kernel/location.py
@@ -1,11 +1,15 @@
 from pydantic import Field
+from typing import ClassVar
 from delivery.utils.ddd_primitives import ValueObject
 import random
 
 
 class Location(ValueObject):
-    X: int = Field(..., ge=1, le=10)
-    Y: int = Field(..., ge=1, le=10)
+    min_location: ClassVar[int] = 1
+    max_location: ClassVar[int] = 10
+
+    X: int = Field(..., ge=min_location, le=max_location)
+    Y: int = Field(..., ge=min_location, le=max_location)
 
     def distance_to(self, target_location: "Location") -> int:
         distance = abs(target_location.X - self.X) + abs(target_location.Y - self.Y)
